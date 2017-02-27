8d02752a47bd4aeb31f8d3b54bc3446c4bc74c0d
Eli Rose <really.eli@gmail.com>
2017-02-27 10:45:56 -0500

    Code style changes.

--- lib/typeform_data/utils.rb
+++ lib/typeform_data/utils.rb
@@ -3,9 +3,9 @@ module Utils
 
   # Repeats the block until it succeeds or a limit is reached, waiting twice as long as it
   # previously did after each failure.
-  # @param rescued_exceptions [Class] Subclasses of Exception.
-  # @param max_retries [Integer]
-  # @param initial_wait [Integer] In seconds.
+  # @param [Class] Subclasses of Exception.
+  # @param [Integer]
+  # @param [Integer] In seconds.
   def self.retry_with_exponential_backoff(rescued_exceptions, max_retries: 5, initial_wait: 1)
     seconds_to_wait = initial_wait
 
