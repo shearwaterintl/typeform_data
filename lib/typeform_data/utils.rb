d4fd7cd9432f632bd9fef4c0025b5f5bd4db10b6
Eli Rose <really.eli@gmail.com>
2017-02-27 12:35:56 -0500

    Include logging.

--- lib/typeform_data/utils.rb
+++ lib/typeform_data/utils.rb
@@ -3,18 +3,19 @@ module Utils
 
   # Repeats the block until it succeeds or a limit is reached, waiting twice as long as it
   # previously did after each failure.
-  # @param [Class] Subclasses of Exception.
+  # @param [TypeformData::Config]
+  # @param [Array<Class>] Subclasses of Exception.
   # @param [Integer]
   # @param [Integer] In seconds.
-  def self.retry_with_exponential_backoff(rescued_exceptions, max_retries: 5, initial_wait: 1)
+  def self.retry_with_exponential_backoff(config, retry_exceptions, max_retries: 5, initial_wait: 1)
     seconds_to_wait = initial_wait
 
     max_retries.times do |iteration|
       begin
         break yield
-      rescue *rescued_exceptions
-        # TODO: Allow the client to pass in a logger, and use it here.
-        puts "Retry. Waiting #{seconds_to_wait}s, attempt #{iteration} of #{max_retries}."
+      rescue *retry_exceptions
+        config.logger.warn "Retry. Waiting #{seconds_to_wait}s, attempt #{iteration} of "\
+          "#{max_retries}."
 
         sleep seconds_to_wait
         seconds_to_wait *= 2
